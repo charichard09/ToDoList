330e70c732a2fd0412e886acf19217176a99f18b
Richard <charichard09@gmail.com>
2022-12-21 07:44:20 -0800

    finish items, add categories table

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,28 +1,72 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace ToDoList.Controllers
-{ 
-  public class ItemsController : Controller
+
+namespace ToDoList.Controllers;
+
+public class ItemsController : Controller
+{
+  private readonly ToDoListContext _db;
+
+  public ItemsController(ToDoListContext db)
   {
-
-    [HttpGet("/categories/{categoriesId}/items/new")]
-    public ActionResult New(int categoriesId)
-    {
-        Category category = Category.Find(categoriesId);
-        return View(category);
-    }
-
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
-    {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
-      return View(model);
-    }
+    _db = db;
   }
+
+  public ActionResult Index()
+  {
+    List<Item> model = _db.Items.ToList();
+    return View(model);
+  }
+
+  public ActionResult Create()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public ActionResult Create(Item item)
+  {
+    _db.Items.Add(item);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
+
+  public ActionResult Details(int id)
+  {
+    Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+    return View(thisItem);
+  }
+
+  public ActionResult Edit(int id)
+  {
+    Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+    return View(thisItem);
+  }
+
+  [HttpPost]
+  public ActionResult Edit(Item item)
+  {
+    _db.Items.Update(item);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
+
+    public ActionResult Delete(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 }
+
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -41,45 +41,45 @@ namespace ToDoList
       #endregion
       
       #region ToDoList business logic Week8
-      Console.WriteLine("To Do List");
-      Console.WriteLine("--------------");
-      bool finished = false;
-      while (!finished)
-      {
-        Console.WriteLine("Would you like to add an item, view your list, or exit? (Add/View/Exit)");
-        string userResponse = Console.ReadLine().ToUpper(); 
-        if (userResponse == "ADD")
-        {
-          //take user input
-          //process and return to start of loop
-          Console.WriteLine("Enter the description for the new item.");
-          string description = Console.ReadLine();
-          Item newItem = new Item(description);
-          Console.WriteLine($"{description} has been added to your list.");
-          finished = IsFinished();
-        }
-        else if (userResponse == "VIEW")
-        {
-          // T newList =              // { Item }
-          List<Item> newList = Item.GetAll();
-          int counter = 1;
-          if (newList.Count == 0)
-          {
-            Console.WriteLine("You don't have anything to do, loser. Get a life");
-            finished = IsFinished();
-          }
-        else
-          {
-            foreach (Item toDoItem in newList)
-            {
-              // 1045: to access description, added .Description property to toDoItem iteration
-              Console.WriteLine($"{counter}. {toDoItem.Description}");
-              counter++;
-            }
-            finished = IsFinished();
-          }
-        }
-      }
+      // Console.WriteLine("To Do List");
+      // Console.WriteLine("--------------");
+      // bool finished = false;
+      // while (!finished)
+      // {
+      //   Console.WriteLine("Would you like to add an item, view your list, or exit? (Add/View/Exit)");
+      //   string userResponse = Console.ReadLine().ToUpper(); 
+      //   if (userResponse == "ADD")
+      //   {
+      //     //take user input
+      //     //process and return to start of loop
+      //     Console.WriteLine("Enter the description for the new item.");
+      //     string description = Console.ReadLine();
+      //     Item newItem = new Item(description);
+      //     Console.WriteLine($"{description} has been added to your list.");
+      //     finished = IsFinished();
+      //   }
+      //   else if (userResponse == "VIEW")
+      //   {
+      //     // T newList =              // { Item }
+      //     List<Item> newList = Item.GetAll();
+      //     int counter = 1;
+      //     if (newList.Count == 0)
+      //     {
+      //       Console.WriteLine("You don't have anything to do, loser. Get a life");
+      //       finished = IsFinished();
+      //     }
+      //   else
+      //     {
+      //       foreach (Item toDoItem in newList)
+      //       {
+      //         // 1045: to access description, added .Description property to toDoItem iteration
+      //         Console.WriteLine($"{counter}. {toDoItem.Description}");
+      //         counter++;
+      //       }
+      //       finished = IsFinished();
+      //     }
+      //   }
+      // }
       #endregion
     }
 
